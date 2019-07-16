eb1504dadbce8c5b187cf0a28a2ff925e6e5b926
katharina <kat.polman@gmail.com>
2019-07-16 17:17:13 +0200

    secret number game with saved file

---
+++ secret_number_objects.py
@@ -0,0 +1,29 @@
+import json
+import random
+import datetime
+
+secret = random.randint(1, 1)
+attempts = 0
+guess = 0
+
+
+with open("score.txt", "r") as score_file:
+    best_score_list = json.loads(score_file.read())
+    print("Top score: " + str(best_score_list))
+
+while guess != secret:
+    guess = int(input("Guess the secret number (between 1 and 30): "))
+    attempts += 1
+
+    if guess == secret:
+        best_score_list.append({"attempts": attempts, "date": str(datetime.datetime.now())})
+        print("Congratulations - you've guessed it!!")
+
+        with open("score.txt", "w") as score_file:
+            score_file.write(json.dumps(best_score_list))
+
+
+
+
+    else:
+        print("Sorry, your guess wasn't correct, the secret number is not " + str(guess))
